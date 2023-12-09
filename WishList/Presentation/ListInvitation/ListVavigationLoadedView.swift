279f3ae493fe7136bb5c99c0abd815b3504fe954
Quentin Cornu <contact@developpeurlibre.com>
2023-12-09 12:07:06 +0100

    Create ListNavigationConfirmationView

--- WishList/Presentation/ListInvitation/ListVavigationLoadedView.swift
+++ WishList/Presentation/ListInvitation/ListVavigationLoadedView.swift
@@ -13,38 +13,45 @@ struct ListVavigationLoadedView: View {
     let userName: String
     let list: PresentList
     
+    @State private var isShowingConfirmationView = false
+    
     var body: some View {
-        VStack(spacing: 40) {
-            Text("\(userName) a partagé une liste avec toi !")
-                .font(.system(size: 23))
-                .bold()
-                .multilineTextAlignment(.center)
-            WishListPreviewCell(list: list)
-            VStack {
-                MainButton(title: "Rejoindre", style: .plain, isActive: .constant(true)) {
-                    
-                }
-                Text("\(userName) ne connaîtra pas les cadeaux que tu choisis pour lui.")
-                    .font(.callout)
-                    .foregroundStyle(.gray)
+        NavigationStack {
+            VStack(spacing: 40) {
+                Text("\(userName) a partagé une liste avec toi !")
+                    .font(.system(size: 23))
+                    .bold()
                     .multilineTextAlignment(.center)
-                    .padding()
+                WishListPreviewCell(list: list)
+                VStack {
+                    MainButton(title: "Rejoindre", style: .plain, isActive: .constant(true)) {
+                        isShowingConfirmationView = true
+                    }
+                    Text("\(userName) ne connaîtra pas les cadeaux que tu choisis pour lui.")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
             }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-        .padding()
-        .background(.primaryBackground)
-        .overlay(alignment: .topTrailing) {
-            Button(action: {
-                dismiss()
-            }, label: {
-                Image(systemName: "multiply")
-                    .imageScale(.large)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.gray)
-                    .padding()
-            })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .padding()
+            .background(.primaryBackground)
+            .overlay(alignment: .topTrailing) {
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Image(systemName: "multiply")
+                        .imageScale(.large)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
+                        .padding()
+                })
+            }
+            .navigationDestination(isPresented: $isShowingConfirmationView) {
+                ListNavigationConfirmationView(dismissStack: { dismiss() })
+            }
         }
     }
 }
